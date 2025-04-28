56286df7e7837a36a75ffe1813b58b11c04de848
Samuel-134 <samuelbrinko380@gmail.com>
2025-04-29 00:41:44 +0200

    fix: arduino print values fix

--- arduinoscript.js
+++ arduinoscript.js
@@ -1,48 +1,73 @@
 const pb = new PocketBase("https://pocasicko.pockethost.io");
-pb.autoCancellation(false)
+pb.autoCancellation(false);
 
-const days = ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok"]
-const cloudiness = ["Jasno", "Polojasno", "Polooblacno", "Oblacno"]
+const days = ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok"];
+const cloudiness = ["Jasno", "Polojasno", "Polooblacno", "Oblacno"];
 
+const outDevice = "hmlz37j3qt4j2et";
+const inDevice = "d39pf9b4l235921";
 
 const elements = {
     tempUnit: document.getElementById("temp-unit"),
-    tempField: document.getElementById("field-temp"),
-    pressureField: document.getElementById("field-pressure"),
-    humidityField: document.getElementById("field-humidity"),
-    lightField: document.getElementById("field-light"),
+    tempUnitOut: document.getElementById("temp-unit-out"),
+
+    tempOut: document.getElementById("Out-temp"),
+    pressureOut: document.getElementById("Out-pressure"),
+    humidityOut: document.getElementById("Out-humidity"),
+    lightOut: document.getElementById("Out-light"),
+
+    tempIn: document.getElementById("In-temp"),
+    pressureIn: document.getElementById("In-pressure"),
+    humidityIn: document.getElementById("In-humidity"),
+    lightIn: document.getElementById("In-light"),
+
     date: document.getElementById("date-time"),
     lightStatus: document.getElementById("light-status"),
+};
+
+async function fetchDeviceData(deviceId) {
+    const list = await pb.collection("measurements").getList(1, 1, {
+        sort: '-created',
+        filter: `device = "${deviceId}"`
+    });
+
+    return list.items[0];
 }
 
-
 async function pull() {
-    const now = new Date(Date.now())
-    elements.date.innerHTML = `${days[now.getDay()]}, ${now.getHours()}:${(now.getMinutes()).toString().padStart(2, '0')}`
-    const items = (await pb.collection("measurements").getList(1, 1, {
-        sort:'-created'
-    })).items
-    if(!items || items.length == 0) return
+    const now = new Date(Date.now());
+    elements.date.innerHTML = `${days[now.getDay()]}, ${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
 
-    const first = items[0]
+    const Out = await fetchDeviceData(outDevice);
+    const In = await fetchDeviceData(inDevice);
 
-    elements.tempUnit.innerHTML = currentUnit
-    elements.tempField.innerHTML = `${currentUnit.includes("C") ? first.temperature.toFixed(1) : (first.temperature * 1.8 +32).toFixed(1)}`
-    elements.pressureField.innerHTML = `${first.pressure.toFixed(1)}`
-    elements.humidityField.innerHTML = `${first.humidity.toFixed(1)}%`
-    elements.lightField.innerHTML = `${Math.max(Math.min((100 - first.light.toFixed(2)), 100), 0)}%`
-    elements.lightStatus.innerHTML = cloudiness[Math.min(Math.floor((100 - first.light) / (100 / cloudiness.length)), cloudiness.length-1)]
+    if (!Out || !In) return;
+
+    elements.tempUnit.innerHTML = currentUnit;
+    elements.tempUnitOut.innerHTML = currentUnit;
+
+    elements.tempOut.innerHTML = `${currentUnit.includes("C") ? Out.temperature.toFixed(1) : (Out.temperature * 1.8 + 32).toFixed(1)}`;
+    elements.pressureOut.innerHTML = `${Out.pressure.toFixed(1)}`;
+    elements.humidityOut.innerHTML = `${Out.humidity.toFixed(1)}%`;
+    elements.lightOut.innerHTML = `${Out.light.toFixed(1)}`;
+
+    elements.lightStatus.innerHTML = `lm`;
+
+    elements.tempIn.innerHTML = `${currentUnit.includes("C") ? In.temperature.toFixed(1) : (In.temperature * 1.8 + 32).toFixed(1)}`;
+    elements.pressureIn.innerHTML = `${In.pressure.toFixed(1)}`;
+    elements.humidityIn.innerHTML = `${In.humidity.toFixed(1)}%`;
+    elements.lightIn.innerHTML = `${In.light.toFixed(1)}`;
 }
 
 $('#units').children().on("click", function() {
-    currentUnit = this.innerHTML
+    currentUnit = this.innerHTML;
 
-    $('#units').children().removeClass("active")
-    $(this).addClass("active")
+    $('#units').children().removeClass("active");
+    $(this).addClass("active");
 
-    pull()
-})
-$('#units').children().first().click()
+    pull();
+});
+$('#units').children().first().click();
 
-pull()
-setInterval(() => pull(), 1000)
+pull();
+setInterval(() => pull(), 1000);
